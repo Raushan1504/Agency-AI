69b60992347403f18b67d2376dc3ed8dbb73626f
Raushan1504 <rashukumar1504@gmail.com>
2026-05-28 15:31:36 +0100

    Initial commit: Agency.AI website with navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const Navbar = ({theme,setTheme}) => {
+
+return (
+ <div className='flex justify-between items-center px-4
+ sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20
+ backdrop-blur-xl font-medium bg-white/80 dark:bg-gray-900/70'>
+<img src = {theme === 'dark' ? assets.logo_dark: assets.logo} className='w-32 sm:w-40' alt = 'logo'/>
+    <div className='dark:text-white sm:text-sm flex flex-row items-center justify-center gap-6 text-gray-700 transition-all max-sm:hidden'>
+            <a href="#" className='hover:text-primary transition-colors'>Home</a>
+            <a href="#services" className='hover:text-primary transition-colors'>Services</a>
+            <a href="#OurWork" className='hover:text-primary transition-colors'>Our Work</a>
+            <a href="#contact-us" className='hover:text-primary transition-colors'>Contact Us</a>
+    </div>
+    <div>
+        <a href="#contact-us" className='text-sm flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full cursor-pointer hover:scale-103 transition-all max-sm:hidden'>
+          Connect <img src={assets.arrow_icon} width={14} alt="" />
+        </a>
+    </div>
+
+</div>
+
+
+)
+}
+
+export default Navbar;
